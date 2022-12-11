3a0b99d609cab4e9b449ac2675d1cecfb0cc819e
root <root@skarvelis.gr>
2022-12-11 22:17:54 +0200

    jsdom overrides

--- src/index.ts
+++ src/index.ts
@@ -8,20 +8,25 @@ declare global {
 
 global.excalibur_ver = "0.27.0";
 
+//webpack uses global self than this, so we need to set it for jsdom to work
 (global as any).self = global;
 
+const options = {
+  pretendToBeVisual: true,
+  userAgent: "excalibur/jsdom",
+};
+const dom = new JSDOM(`<!DOCTYPE html><html><head></head><body><p>Hello Swords!</p><body></html>`,options)
 
-const dom = new JSDOM(`<!DOCTYPE html><html><head></head><body><p>Hello Swords!</p><body></html>`)
-
-const emptyFn = () => {}
 const document = dom.window.document
 const window = dom.window
 console.log(dom.window.document.querySelector("p")!.textContent);
 
 global.document = document
+
 global.window =  window as any
-//@ts-ignore polifyll for matchMedia
-global.window.matchMedia =  () => {
+
+//@ts-ignore jsdom doesn't have this method
+global.window.matchMedia = () => {
   return {
   matches : false,
   addListener : function() {},
@@ -29,15 +34,23 @@ global.window.matchMedia =  () => {
   };
   };
 
-//@ts-ignore use canvas for dommatrix
 global.DOMMatrix = Canvas.DOMMatrix
-  
 global.Window = dom.window.constructor as any
 
-global.window.Element = undefined as any
-global.matchMedia = emptyFn as any
+global.navigator = { userAgent: 'excalibur/jsdom' } as any
 
-global.navigator = { userAgent: 'excalibur' } as any
-
+//Cudos to jsdm issue #2154
 global.Image = window.Image
+Object.defineProperty((global as any).Image.prototype, 'decode', {
+  get() {
+    return () => Promise.resolve()
+  },
+});
+
 global.XMLHttpRequest = FakeXMLHttpRequest as any
+
+/* TODO
+to make it work on Loader.ts of excaliburjs at `Loader draw function`
+i've comment out the ctx.drawImage
+so need to find a way to make it work without commenting it
+*/
