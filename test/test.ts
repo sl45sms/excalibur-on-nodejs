ec6110c99e1ef8b1f72ef5b2629212d2d748c337
root <root@skarvelis.gr>
2022-12-11 22:18:08 +0200

    some more tests

--- test/test.ts
+++ test/test.ts
@@ -1,5 +1,9 @@
 import "../src/index";
-import { Actor,DisplayMode, CollisionType, Color, Engine, vec } from "excalibur";
+import { Text,Loader, Actor, DisplayMode, CollisionType, Color, Engine, vec } from "excalibur";
+
+const loader = new Loader();
+loader.playButtonText = "Sword";
+loader.backgroundColor = ""+Color.Green;
 
 const game = new Engine({
     width: 800,
@@ -9,32 +13,39 @@ const game = new Engine({
       width: 800,
       height: 600
     },
-    displayMode: DisplayMode.FitScreen
+    configurePerformanceCanvas2DFallback:{
+      allow:true,
+      showPlayerMessage:false
+    },
+    displayMode: DisplayMode.FitScreen,
+    suppressPlayButton: true,
+    suppressMinimumBrowserFeatureDetection: true,
   });
 
 const actor = new Actor({
     name: 'red',
-    pos: vec(150, 150),
+    pos: vec(100, 100),
     width: 50,
     height: 50,
     color: Color.Red,
     collisionType: CollisionType.Active
 });
 
-// move the actor 15px to the right every frame
-actor.vel.x = 15
+//move the actor 15px to the right every frame
+actor.vel.x = 15;
 
-//when the player goes off screen, wrap them to the other side
-actor.on('precollision', (ev) => {
-     console.log(ev)
-    }
-);
-
+//when the player goes off screen, show a message
+actor.on('exitviewport', () => {
+    //display a message to prove the engine works
+    console.log('off screen');
+});
 
 game.add(actor);
-game.start().then(function () {
+game.start(loader).then(function () {
   // ready, set, go!
   console.log("ready, set, go!");
-  console.log(window.document.getElementsByTagName("canvas")[0].toDataURL());
+  setTimeout(() => {
+    //on 10 seconds, get the canvas data to prove a red rectangle is on the screen
+    console.log(window.document.getElementsByTagName("canvas")[0].toDataURL());    
+  }, 10000);
 })
-
